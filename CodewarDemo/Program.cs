5964a017a79349bea1f468dc92f7a8816e905904
pawankamdi <pawankamdi26@gmail.com>
2023-02-22 11:42:01 +0530

    Add project files.

---
+++ CodewarDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodewarDemo
+{
+    //Consider an array/list of sheep where some sheep may be missing from their place. 
+    //We need a function that counts the number of sheep present in the array (true means present).
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Kata k = new Kata();
+            bool[] sheeps = new bool[2];
+            sheeps[0] = false;
+            sheeps[1] = true;
+            k.CountSheeps(sheeps);
+            foreach (bool res in sheeps)
+            {
+                Console.WriteLine(res);
+            }
+                
+          
+            Console.ReadLine();
+        }
+        public  class Kata
+        {
+            public  int CountSheeps(bool[] sheeps)
+            {
+                return sheeps.Count(sheep => sheep);
+            }
+            
+        }
+        
+    }
+    
+}
